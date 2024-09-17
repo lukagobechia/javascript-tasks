c2acd9ba9ceb75d282f55b8047a228e072320b1a
Luka Gobechia <lukagobechiaa@gmail.com>
2024-09-17 14:55:59 +0400

    second commit

--- task1.js
+++ task1.js
@@ -1,19 +1,20 @@
 function reverseWord(string) {
-    let result = [];
-    let word = "";
-    let i = 0;
-    while (i <= string.length) {
-      if (string[i] == " " || i == string.length) {
-        if(string[i-1] != " ")
-        result.push(word);
-        word = "";
-      } else word += string[i];
-      i++;
-    }
-    let reversedString = "";
-    for (let j = result.length - 1; j >= 0; j--)
-      reversedString += result[j] + " ";
-    return reversedString;
+  let result = [];
+  let word = "";
+  let i = 0;
+  while (i <= string.length) {
+    if (string[i] == " " || i == string.length) {
+      if (string[i - 1] != " ") result.push(word);
+      word = "";
+    } else word += string[i];
+    i++;
   }
-  console.log(reverseWord("Hello       World   ")); // handles all the spaces
-  
+  let reversedString = "";
+  for (let j = result.length - 1; j >= 0; j--)
+    reversedString += result[j] + " ";
+  return reversedString;
+}
+console.log(reverseWord("Hello       World   ")); // handles all the spaces
+
+// input example : "Hello       World   "
+// output : "Wordl Hello"
